edc7f313869b50c9b64405942cea45d386bef5ec
Itumeleng Malemetja <malematjai@gmail.com>
2023-11-28 23:48:52 -0800

    fixing output

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[106:121] + str[136:173] + str[0:6]
+print(str)
