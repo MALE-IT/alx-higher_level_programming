f0efe745a764ef518638132bee0ef878ce8994bf
Itumeleng Malemetja <malematjai@gmail.com>
2023-11-30 04:35:44 -0800

    fixing the requirement

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
+    import sys
     result = 0
-    for i in sys.argv[1:]:
-        result += int(i)
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
     print(result)
