fb60204bd680c160d5b7305a9379697b4138fd7c
Itumeleng Malemetja <malematjai@gmail.com>
2023-12-19 03:07:16 -0800

    a class Square that defines a square by: (based on 5-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,15 +60,14 @@ class Square:
     def area(self):
         """Calculates and returns the current square area."""
         return self.__size ** 2
+
     def my_print(self):
-    """Prints the square with the # character."""
-    if self.__size == 0:
-        print("")
-        return
-
-    [print("") for i in range(0, self.__position[1])]
-
-    for i in range(0, self.__size):
-        [print(" ", end="") for j in range(0, self.__position[0])]
-        [print("#", end="") for k in range(0, self.__size)]
-        print("")
+        """Prints the square with the # character."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
